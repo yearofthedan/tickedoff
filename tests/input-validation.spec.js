183f18ddfd8bb73c3a736974f2f9e0452a33d849
Ryan Boucher <ryan.boucher@distributedlife.com>
2016-05-18 08:07:39 +1000

    :clock5: Split up validation tests, added specific minute scenarios

--- tests/input-validation.spec.js
+++ tests/input-validation.spec.js
@@ -3,12 +3,33 @@ var expect = require('chai').expect;
 const checkInput = require('../src/input-validation').checkInput;
 
 describe('input validation', () => {
-    const invalidTime = ['-1', 'one-am', undefined, null, '24:23', '20:60', '9:30', '10:3'];
+  describe('malformed input', () => {
+    const invalidTime = ['-1', 'one-am', undefined, null];
 
     invalidTime.forEach(scenario => {
         it(`should report invalid time when ${scenario} is supplied`, () => {
             expect(() => checkInput(scenario)).to.throw('Invalid time format');
         });
     })
+  });
 
+  describe('hour validation', () => {
+    const invalidTime = ['24:23', '9:30'];
+
+    invalidTime.forEach(scenario => {
+        it(`should report invalid time when ${scenario} is supplied`, () => {
+            expect(() => checkInput(scenario)).to.throw('Invalid time format');
+        });
+    })
+  });
+
+  describe('minute validation', () => {
+    const invalidTime = ['10:-1', '10:fifteen', '10:60', '10:3'];
+
+    invalidTime.forEach(scenario => {
+        it(`should report invalid time when ${scenario} is supplied`, () => {
+            expect(() => checkInput(scenario)).to.throw('Invalid time format');
+        });
+    })
+  });
 })
