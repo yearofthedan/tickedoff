06828695f0bbd3e6d591186413d1a3b0f22a35db
Ryan Boucher <ryan.boucher@distributedlife.com>
2016-05-18 13:12:59 +1000

    :clock830: running script now has responsibility for stdin/out, clock face now delegates to renderer

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
-const asciiClockFace = require('./src/asciiface');
-asciiClockFace();
+
+const readline = require('readline');
+const clockFace = require('./src/clock-face');
+const asciiClockDisplay = require('./src/clock-display');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Enter the time: ', theTime => {
+  process.stdout.write(`${clockFace(theTime, asciiClockDisplay)}\n\r`);
+  rl.close();
+});
---
+++ src/clock-face.js
@@ -0,0 +1,19 @@
+const checkInput = require('./input-validation');
+const mapHourToInterval = require('./time-to-interval-mapping').mapHourToInterval;
+const mapMinuteToInterval = require('./time-to-interval-mapping').mapMinuteToInterval
+const buildIntervals = require('./build-intervals');
+
+const getHourFromTime = theTime => theTime.split(':')[0];
+const getMinutesFromTime = theTime => theTime.split(':')[1];
+
+function clockFace (theTime, renderer) {
+    if (!checkInput(theTime)) {
+        return 'Invalid time. Input must be in hh:mm format e.g. 13:30';
+    }
+
+    const hourHand = mapHourToInterval(getHourFromTime(theTime));
+    const minuteHand = mapMinuteToInterval(getMinutesFromTime(theTime));
+    return renderer(buildIntervals(hourHand, minuteHand));
+}
+
+module.exports = clockFace;
